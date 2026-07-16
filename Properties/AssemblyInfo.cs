badcf819bedf796cd6e110565b849dbcd213deca
T8899J <2088597321@qq.com>
2026-07-16 23:16:23 +0800

    chore: release v1.3.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 // [assembly: Guid("...")]
 
 // 程序集版本信息
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
